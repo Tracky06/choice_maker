2d9fa82183c883adeca0793bffca4bf5bde88c9b
Tracky06 <ro142857@gmail.com>
2025-02-05 10:25:43 +0000

    Completed the job

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,36 @@ import React, { useState } from "react";
 import './App.css';
 
 function App() {
-  const [heading, setHeading] = useState("Welcome, Press Start to roll the die!");
-  const [player1, setPlayer1] = useState(6);
-  const [player2, setPlayer2] = useState(6);
+  const [heading, setHeading] = useState("Welcome to your choice maker, Dr. Cheff!");
+  const [player, setPlayer] = useState(6);
+  const [isRolling, setIsRolling] = useState(false);
 
   function handleClick() {
-    const value1 = Math.floor(Math.random() * 6) + 1;
-    const value2 = Math.floor(Math.random() * 6) + 1;
-
-    if (value1 > value2) {
-      setHeading("Player 1 Wins!");
-    } else if (value1 < value2) {
-      setHeading("Player 2 Wins!");
-    } else {
-      setHeading("Draw!");
-    }  
+    const value = Math.floor(Math.random() * 6) + 1;
     
-    setPlayer1(value1);
-    setPlayer2(value2);
+    setIsRolling(true);
+    setTimeout(() => {
+      setIsRolling(false) 
+      if (value % 2 === 0) {
+        setHeading("Your choice is even!")
+      } else {
+        setHeading("Your choice is odd!")
+      }
+      setPlayer(value);
+    }, 1000);
+
+    
   }
 
-  const imagePath1 = `./images/dice-${player1}.png`;
-  const imagePath2 = `./images/dice-${player2}.png`;  
+  const imagePath = `./images/dice-${player}.png`;
    
   return (
     <div className="main-body">
       <div>
         <h1 className="heading">{heading}</h1>
-        <button className="game-btn permanent-marker-regular" onClick={handleClick}>START</button>
+        <button className="game-btn permanent-marker-regular" onClick={handleClick}>{isRolling? "ROLLING..." : "ROLL"}</button>
         <div className="image-container">
-          <div>Player 1<img src={imagePath1} alt={player1} /></div>
-          <div>Player 2<img src={imagePath2} alt={player2} /></div>
+          <div className={isRolling? "rolling" : ""}><img src={imagePath} alt={player} /></div>
         </div>
       </div>
     </div>
